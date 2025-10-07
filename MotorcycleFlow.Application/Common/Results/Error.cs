a2f937bdcc2bb5ab314bdc5d1f0ed8b7fd9784ab
Rafael069 <santiagorafael069@gmail.com>
2025-10-07 14:26:26 -0300

    Classes de retorno Error e Result

---
+++ MotorcycleFlow.Application/Common/Results/Error.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorcycleFlow.Application.Common.Results
+{
+    public record Error(string Code, string Message)
+    {
+        public static readonly Error None = new(string.Empty, string.Empty);
+        public static readonly Error NotFound = new("NotFound", "Resource not found");
+        public static readonly Error Conflict = new("Conflict", "Resource already exists");
+        public static readonly Error Validation = new("Validation", "Validation error");
+        public static readonly Error InvalidFormat = new("InvalidFormat", "Invalid file format");
+
+        public static Error Failure(string code, string message) => new(code, message);
+
+       
+    }
+}
---
+++ MotorcycleFlow.Application/Common/Results/Result.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MotorcycleFlow.Application.Common.Results
+{
+    public class Result<T>
+    {
+        public bool IsSuccess { get; }
+        public T Value { get; }
+        public Error Error { get; }
+
+        private Result(bool isSuccess, T value, Error error)
+        {
+            IsSuccess = isSuccess;
+            Value = value;
+            Error = error;
+        }
+
+        public static Result<T> Success(T value) => new(true, value, Error.None);
+        public static Result<T> Failure(Error error) => new(false, default, error);
+    }
+}
