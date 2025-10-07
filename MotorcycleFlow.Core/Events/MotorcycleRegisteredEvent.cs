367e061a7289eefeeb562850b11cae11736030a3
Rafael069 <santiagorafael069@gmail.com>
2025-10-07 14:41:22 -0300

    MotorcycleRegisteredEvent

---
+++ MotorcycleFlow.Core/Events/MotorcycleRegisteredEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorcycleFlow.Core.Events
+{
+    public class MotorcycleRegisteredEvent
+    {
+        public Guid MotorcycleId { get; set; }
+        public int Year { get; set; }
+        public string Model { get; set; } 
+        public string LicensePlate { get; set; } 
+        public DateTime RegisteredAt { get; set; }
+    }
+}
