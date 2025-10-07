53d8586ef4484f4ac2145ae3df8f0be4baa8427a
Rafael069 <santiagorafael069@gmail.com>
2025-10-07 14:31:58 -0300

    Program Atualizada

--- MotorcycleFlow.API/Program.cs
+++ MotorcycleFlow.API/Program.cs
@@ -1,6 +1,12 @@
 
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using MotorcycleFlow.Application.Features.Motorcycles.Commands;
+using MotorcycleFlow.Application.Interfaces;
+using MotorcycleFlow.Application.Services;
 using MotorcycleFlow.Infrastructure.Data;
+using MotorcycleFlow.Infrastructure.Repositories;
+using MotorcycleFlow.Infrastructure.Services;
 
 
 namespace MotorcycleFlow.API
@@ -15,12 +21,21 @@ namespace MotorcycleFlow.API
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL")));
 
+            builder.Services.AddScoped<IMotorcycleRepository, MotorcycleRepository>();
+            builder.Services.AddScoped<IDeliveryPersonRepository, DeliveryPersonRepository>();
+            builder.Services.AddScoped<IRentalRepository, RentalRepository>();
+            builder.Services.AddScoped<IRentalCalculatorService, RentalCalculatorService>();
+            builder.Services.AddScoped<IImageStorageService, LocalImageStorageService>();
+            builder.Services.AddScoped<INotificationService, RabbitMQNotificationService>();
+
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddMediatR(typeof(CreateMotorcycleCommand));
 
             var app = builder.Build();
 
@@ -34,7 +49,7 @@ namespace MotorcycleFlow.API
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
+            app.UseStaticFiles();
 
             app.MapControllers();
 
